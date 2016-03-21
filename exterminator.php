2a2225fdfdb506a265f24c239747ef3791566c7a
guy <guy@jetweb.co.il>
2016-03-21 08:43:09 +0200

    Update exterminator.php

--- exterminator.php
+++ exterminator.php
@@ -23,3 +23,25 @@ function exterminator_register_widgets() {
 }
 
 add_action( 'widgets_init', 'exterminator_register_widgets' );
+
+add_action( 'init', 'github_plugin_updater_init' );
+function github_plugin_updater_init() {
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'exterminator',
+			'api_url' => 'https://api.github.com/repos/guyJetweb/exterminator',
+			'raw_url' => 'https://raw.github.com/guyJetweb/exterminator/master',
+			'github_url' => 'https://github.com/guyJetweb/exterminator',
+			'zip_url' => 'https://github.com/guyJetweb/exterminator/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+                          
+		new WP_GitHub_Updater( $config );
+	}
+}
